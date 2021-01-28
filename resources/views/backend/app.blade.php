5e7a87eac44388734da3fb8dc497483ad8a7155b
mansur <yaponomat@gmail.com>
2021-01-28 14:48:34 +0300

    Add admin assets

---
+++ resources/views/backend/app.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="{{ config('app.locale') }}">
+<head>
+  <meta charset="UTF-8">
+  <title>Admin</title>
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+  <script src="{{ mix('js/backend/manifest.js') }}" defer></script>
+  <script src="{{ mix('js/backend/vendor.js') }}" defer></script>
+  <script src="{{ mix('js/backend/app.js') }}" defer></script>
+</head>
+<body>
+  <div id="app"></div>
+</body>
+</html>
