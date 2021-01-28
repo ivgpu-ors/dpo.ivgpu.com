52908b9fa32dbcced4ad939c3fa8d09cc65d240c
mansur <yaponomat@gmail.com>
2021-01-28 14:48:20 +0300

    Config assets compiler

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const path = require('path');
 require('laravel-mix-polyfill');
 
 /*
@@ -12,15 +13,21 @@ require('laravel-mix-polyfill');
  |
  */
 
-mix.ts('resources/js/app.ts', 'public/js').vue()
-   .sass('resources/css/app.scss', 'public/css').options({ postCss: [ require('tailwindcss') ] })
-   .polyfill({
-      enabled: true,
-      useBuiltIns: "usage",
-      targets: false,
-      debug: false,
-    })
-   .extract();
+mix
+  .ts('resources/js/app.ts', 'public/js').vue()
+  .ts('resources/js/backend/app.ts', 'public/js/backend').vue()
+  .alias({
+    '@backend': path.join(__dirname, 'resources/js/backend'),
+    'vue$': path.join(__dirname, 'node_modules/vue/dist/vue.esm-bundler.js')
+  })
+  .sass('resources/css/app.scss', 'public/css').options({ postCss: [require('tailwindcss')] })
+  .polyfill({
+    enabled: true,
+    useBuiltIns: "usage",
+    targets: false,
+    debug: false,
+  })
+  .extract();
 
 if (mix.inProduction()) {
   mix.version();
