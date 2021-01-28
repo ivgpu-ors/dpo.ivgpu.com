777f0defb619fa56e3ad4e87331285a51a2900a0
mansur <yaponomat@gmail.com>
2021-01-28 12:56:08 +0300

    Add courses controller

---
+++ app/Http/Controllers/Api/CourseController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Course;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class CourseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse
+    {
+        $courses = Course::all(['id', 'name']);
+        return response()->json($courses);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function store(Request $request): JsonResponse
+    {
+        $course = Course::create($request->post());
+        return response()->json($course);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Course $course
+     * @return JsonResponse
+     */
+    public function show(Course $course): JsonResponse
+    {
+        return response()->json($course);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param Course $course
+     * @return JsonResponse
+     */
+    public function update(Request $request, Course $course): JsonResponse
+    {
+        $course->fill($request->post())->save();
+        return response()->json($course);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Course $course
+     * @return JsonResponse
+     * @throws Exception
+     */
+    public function destroy(Course $course): JsonResponse
+    {
+        $course->delete();
+        return response()->json();
+    }
+}
