606d98a5a5446f2539f354597adea59b7d8784e7
mansur <yaponomat@gmail.com>
2021-01-29 12:27:54 +0300

    Config tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,23 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: '#9bb8d4',
+          DEFAULT: '#6699cc',
+          dark: '#406b96'
+        },
+        accent: {
+          light: '#f29992',
+          DEFAULT: '#fe5f55',
+          dark: '#d32519'
+        },
+        dark: '#001427',
+        light: '#EEF4ED',
+        jasmine: '#F4D58D'
+      }
+    },
   },
   variants: {
     extend: {},
