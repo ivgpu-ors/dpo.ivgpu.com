f75d3d44c0b78b6a644d0de80de26723ecc16e94
mansur <yaponomat@gmail.com>
2021-02-03 15:21:15 +0300

    Add medium editor to backend

---
+++ resources/js/backend/components/form/VHtml.vue
@@ -0,0 +1,33 @@
+<template>
+  <label class="block mb-2">
+    <slot/>
+  </label>
+  <div class="mb-4 border border-gray-400 shadow block w-full rounded p-1 focus:ring focus:outline-none">
+    <div v-html="modelValue" ref="editorRef"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, onMounted } from 'vue';
+import * as MediumEditor from 'medium-editor';
+
+export default defineComponent({
+  props: {
+    modelValue: { type: String }
+  },
+  emits: ['update:modelValue'],
+  setup() {
+    const editorRef = ref(null);
+    let editor = null;
+
+    onMounted(() => {
+      editor = new MediumEditor(editorRef.value);
+      console.log(editor);
+    });
+
+    return {
+      editorRef
+    }
+  }
+});
+</script>
--- resources/js/backend/pages/courses/CreateCoursePage.vue
+++ resources/js/backend/pages/courses/CreateCoursePage.vue
@@ -3,22 +3,35 @@
     <h2>Create course</h2>
     <v-input id="name" v-model="name">Название курса</v-input>
     <v-input id="date" v-model="start" type="date">Дата начала</v-input>
+    <v-input id="date" v-model="end" type="date">Дата окончания</v-input>
+    <v-input id="duration" v-model="duration">Длительность курса</v-input>
+    <v-input id="education_time" v-model="education_time">Понадобится для освоения</v-input>
+    <v-html v-model="description">О курсе</v-html>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
 import VInput from "@backend/components/form/VInput.vue";
+import VHtml from "@backend/components/form/VHtml.vue";
 
 export default defineComponent({
-  components: { VInput },
+  components: { VHtml, VInput },
   setup() {
     const name = ref('');
     const start = ref('');
+    const end = ref('');
+    const duration = ref('');
+    const education_time = ref('');
+    const description = ref('Description example');
 
     return {
       name,
-      start
+      start,
+      end,
+      duration,
+      education_time,
+      description
     }
   }
 });
