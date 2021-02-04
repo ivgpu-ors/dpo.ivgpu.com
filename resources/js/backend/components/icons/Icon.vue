cd1a7fc9641c60f3149ce4c91a47c54ef3d9a037
mansur <yaponomat@gmail.com>
2021-02-04 15:04:56 +0300

    Add base icon component

---
+++ resources/js/backend/components/icons/Icon.vue
@@ -0,0 +1,18 @@
+<template>
+  <component :is="`icon-${icon}`" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import IconPlus from "@backend/components/icons/IconPlus.vue";
+import IconSave from "@backend/components/icons/IconSave.vue";
+
+export default defineComponent({
+  components: {
+    IconPlus, IconSave
+  },
+  props: {
+    icon: { type: String }
+  }
+});
+</script>
