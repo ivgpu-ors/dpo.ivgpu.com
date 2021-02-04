d129ffcfb6355bf095a3bb77700214b08fc434d6
mansur <yaponomat@gmail.com>
2021-02-04 11:43:26 +0300

    Styling sidebar

---
+++ resources/js/backend/components/SidebarLink.vue
@@ -0,0 +1,16 @@
+<template>
+  <router-link :to="to" class="flex items-center h-14 p-2 border-b border-gray-500 last:border-b-0">
+    <div class="w-6 lg:mr-2"><slot name="icon" /></div>
+    <span class="hidden lg:block"><slot /></span>
+  </router-link>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    to: { type: String, required: true },
+  },
+});
+</script>
