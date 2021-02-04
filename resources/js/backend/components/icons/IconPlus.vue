eb7c428ee1d51dca63a7f5fcd7927b67c6a39fb7
mansur <yaponomat@gmail.com>
2021-02-04 14:47:51 +0300

    Create icons component

---
+++ resources/js/backend/components/icons/IconPlus.vue
@@ -0,0 +1,11 @@
+<template>
+  <img svg-inline src="@svg/icons/plus.svg">
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+
+});
+</script>
