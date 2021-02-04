6a42953b619fe030be79e4b17b2bff90471f3c72
mansur <yaponomat@gmail.com>
2021-02-04 15:05:32 +0300

    Modify link-button component

--- resources/js/backend/components/ui/LinkButton.vue
+++ resources/js/backend/components/ui/LinkButton.vue
@@ -1,17 +1,17 @@
 <template>
   <router-link :to="to" class="py-2 px-4 bg-primary text-light rounded-md inline-flex hover:bg-primary-dark items-center">
-    <component :is="`icon-${icon}`" class="w-4 mr-3" />
+    <icon :icon="icon" class="w-4 mr-3" />
     <slot />
   </router-link>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import IconPlus from "@backend/components/icons/IconPlus.vue";
+import Icon from "@backend/components/icons/Icon.vue";
 
 export default defineComponent({
   components: {
-    IconPlus,
+    Icon,
   },
   props: {
     to: { type: String, required: true },
