0f65e4ec659173599e1af14741e1d1b078cb5547
mansur <yaponomat@gmail.com>
2021-02-04 14:48:17 +0300

    Create link button

---
+++ resources/js/backend/components/ui/LinkButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <router-link :to="to" class="py-2 px-4 bg-primary text-light rounded-md inline-flex hover:bg-primary-dark items-center">
+    <component :is="`icon-${icon}`" class="w-4 mr-3" />
+    <slot />
+  </router-link>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import IconPlus from "@backend/components/icons/IconPlus.vue";
+
+export default defineComponent({
+  components: {
+    IconPlus,
+  },
+  props: {
+    to: { type: String, required: true },
+    icon: { type: String }
+  }
+});
+</script>
