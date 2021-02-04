6eff088b3966a58c9885d97fa60f6b067e9153dd
mansur <yaponomat@gmail.com>
2021-02-04 15:03:43 +0300

    Create VButton.vue

---
+++ resources/js/backend/components/form/VButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button class="py-2 px-4 bg-primary text-light rounded-md inline-flex hover:bg-primary-dark items-center">
+    <icon :icon="icon" class="w-4 mr-3" />
+    <slot />
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Icon from "@backend/components/icons/Icon.vue";
+
+export default defineComponent({
+  components: {
+    Icon,
+  },
+  props: {
+    icon: { type: String }
+  }
+});
+</script>
