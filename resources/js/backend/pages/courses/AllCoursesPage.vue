693045ae31622ea4b1ee5168fbb3e1dea4e93eec
mansur <yaponomat@gmail.com>
2021-02-04 14:52:13 +0300

    replace add button in AllCoursesPage.vue

--- resources/js/backend/pages/courses/AllCoursesPage.vue
+++ resources/js/backend/pages/courses/AllCoursesPage.vue
@@ -1,14 +1,12 @@
 <template>
-  <router-link to="/admin/courses/create" class="p-2 bg-primary text-light rounded-md inline-flex hover:bg-primary-dark items-center">
-    <img svg-inline src="@svg/icons/plus.svg" class="h-4 mr-2">
-    Add
-  </router-link>
+  <link-button to="/admin/courses/create" icon="plus">Add</link-button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import LinkButton from "@backend/components/ui/LinkButton.vue";
 
 export default defineComponent({
-
+  components: { LinkButton }
 });
 </script>
