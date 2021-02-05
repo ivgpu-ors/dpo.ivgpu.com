b3585991dd08755eb4ddb0f7e4fe0de4028e3b25
mansur <yaponomat@gmail.com>
2021-02-05 09:35:15 +0300

    Replace link-button to v-button in all-courses page

--- resources/js/backend/pages/courses/AllCoursesPage.vue
+++ resources/js/backend/pages/courses/AllCoursesPage.vue
@@ -1,12 +1,15 @@
 <template>
-  <link-button to="/admin/courses/create" icon="plus">Add</link-button>
+  <div class="p-3">
+    <h1>Courses</h1>
+    <v-button route="/admin/courses/create" icon="plus">Add</v-button>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import LinkButton from "@backend/components/ui/LinkButton.vue";
+import VButton from "@backend/components/form/VButton.vue";
 
 export default defineComponent({
-  components: { LinkButton }
+  components: { VButton  }
 });
 </script>
