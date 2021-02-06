55ecff697b12fbb3bbf5e982ed8cf312622179da
Mansur <yaponomat@gmail.com>
2021-02-06 22:15:03 +0300

    Update VInputSelect behavior

--- resources/js/backend/components/form/VInputSelect.vue
+++ resources/js/backend/components/form/VInputSelect.vue
@@ -5,18 +5,21 @@
     </label>
 
     <div class="mb-4 border border-gray-400 shadow block w-full rounded p-1 absolute bg-white z-0 focus:outline-none"
-         :class="{ ring: focus }" tabindex="0" @focus="focus = true" @blur="focus = false">
+         :class="{ ring: focus }" tabindex="0" @focus="setFocusToInput" @blur="focus = false" @keydown="keyPressHandler">
 
       <label :for="id" v-if="selected" v-show="!focus" class="absolute">{{ selected[valueKey] }}</label>
 
       <input :id="id" type="text" :value="search" @input="$emit('update:search', $event.target.value)"
-             class="w-full focus:outline-none" tabindex="-1" @focus="focus = true">
+             class="w-full focus:outline-none" tabindex="-1" @focus="focus = true" ref="inputRef">
 
       <ul v-if="focus">
         <li v-for="option in options" :key="option[idKey]" :value="option[idKey]"
-            class="leading-6 cursor-pointer hover:bg-blue-200"
-            @click="select(option)">
+            class="leading-6 cursor-pointer hover:bg-blue-200" @click="select(option)">
           {{ option[valueKey] }}
+        </li>
+        <li class="flex h-12 items-center border-t border-gray-200">
+          <kbd class="bg-gray-200 rounded-lg px-2">Ctrl + Enter</kbd>
+          <button class="italic hover:underline ml-2" tabindex="-1" @click="addElement">Добавить элемент</button>
         </li>
       </ul>
 
@@ -27,33 +30,60 @@
 <script lang="ts">
 import { defineComponent, onMounted, onUnmounted, ref, PropType } from 'vue';
 import { nanoid } from "nanoid";
+import VButton from "@backend/components/form/VButton.vue";
 
 type Option = {
   [key: string]: string
 }
 
 export default defineComponent({
+  components: { VButton },
   props: {
     options: { type: Array as PropType<Option[]>, required: true },
     idKey: { type: String, default: 'id' },
     valueKey: { type: String, default: 'value' },
-    modelValue: { type: [Number, String], required: true },
+    modelValue: { type: [Number, String] },
     search: { type: String },
   },
 
-  emits: ['update:modelValue', 'update:search'],
+  emits: ['update:modelValue', 'update:search', 'add'],
 
   setup(props, { emit }) {
     const id = nanoid();
     const focus = ref(false);
     const selected = ref<Option | undefined>(props.options.find(o => o[props.idKey] === props.modelValue));
 
-    const blurFocus = () => focus.value = false;
+    const blurFocus = () => {
+      focus.value = false;
+      if (inputRef.value) {
+        inputRef.value.blur();
+      }
+    }
 
     const select = (option: Option) => {
       blurFocus();
       selected.value = option;
       emit('update:modelValue', option[props.idKey]);
+    }
+
+    const addElement = () => {
+      emit('add');
+    }
+
+    const inputRef = ref<HTMLInputElement | null>(null);
+    const setFocusToInput = () => {
+      focus.value = true;
+      if (inputRef.value) {
+        inputRef.value.focus();
+      }
+    }
+
+    const keyPressHandler = (event: KeyboardEvent) => {
+      if (event.key === 'Tab') {
+        blurFocus();
+      } else if (event.key === 'Enter' && event.ctrlKey) {
+        addElement();
+      }
     }
 
     onMounted(() => {
@@ -65,10 +95,14 @@ export default defineComponent({
     });
 
     return {
+      inputRef,
       id,
       focus,
       selected,
       select,
+      setFocusToInput,
+      keyPressHandler,
+      addElement,
     }
   }
 });
