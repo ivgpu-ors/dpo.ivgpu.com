70a0ef484c5318ba7b8161127a9f12e8c771cf0d
Mansur <yaponomat@gmail.com>
2021-02-07 10:04:22 +0300

    VInputSelect.vue select by keyboard

--- resources/js/backend/components/form/VInputSelect.vue
+++ resources/js/backend/components/form/VInputSelect.vue
@@ -5,7 +5,7 @@
     </label>
 
     <div class="mb-4 border border-gray-400 shadow block w-full rounded p-1 absolute bg-white z-0 focus:outline-none"
-         :class="{ ring: focus }" tabindex="0" @focus="setFocusToInput" @blur="focus = false" @keydown="keyPressHandler">
+         :class="{ ring: focus }" tabindex="0" @focus="setFocus" @blur="focus = false" @keydown="keyPressHandler">
 
       <label :for="id" v-if="selected" v-show="!focus" class="absolute">{{ selected[valueKey] }}</label>
 
@@ -13,8 +13,8 @@
              class="w-full focus:outline-none" tabindex="-1" @focus="focus = true" ref="inputRef">
 
       <ul v-if="focus">
-        <li v-for="option in options" :key="option[idKey]" :value="option[idKey]"
-            class="leading-6 cursor-pointer hover:bg-blue-200" @click="select(option)">
+        <li v-for="option in options" :key="option[idKey]"
+            class="leading-6 cursor-pointer" :class="{ 'bg-blue-200': hover === option[idKey] }" @click="select(option[idKey])" @mouseover="hoverOn(option[idKey])">
           {{ option[valueKey] }}
         </li>
         <li class="flex h-12 items-center border-t border-gray-200">
@@ -52,6 +52,7 @@ export default defineComponent({
     const id = nanoid();
     const focus = ref(false);
     const selected = ref<Option | undefined>(props.options.find(o => o[props.idKey] === props.modelValue));
+    const inputRef = ref<HTMLInputElement | null>(null);
 
     const blurFocus = () => {
       focus.value = false;
@@ -60,29 +61,54 @@ export default defineComponent({
       }
     }
 
-    const select = (option: Option) => {
-      blurFocus();
-      selected.value = option;
-      emit('update:modelValue', option[props.idKey]);
-    }
-
-    const addElement = () => {
-      emit('add');
-    }
-
-    const inputRef = ref<HTMLInputElement | null>(null);
-    const setFocusToInput = () => {
+    const setFocus = () => {
       focus.value = true;
       if (inputRef.value) {
         inputRef.value.focus();
       }
     }
 
+    const select = (id: String) => {
+      blurFocus();
+      selected.value = props.options.find(o => o[props.idKey] === id);
+      emit('update:modelValue', id);
+    }
+
+    const addElement = () => {
+      emit('add');
+    }
+
+    const hover = ref<String | null>(null);
+
+    const hoverOn = (id: String) => {
+      hover.value = id;
+    }
+
+    const hoverNext = () => {
+      const currentId = hover.value ? props.options.findIndex(o => o[props.idKey] === hover.value) : -1;
+      const nextOption = props.options[currentId + 1] ?? props.options[0];
+
+      hover.value = nextOption ? nextOption[props.idKey] : null;
+    }
+
+    const hoverPrev = () => {
+      const currentId = hover.value ? props.options.findIndex(o => o[props.idKey] === hover.value) : props.options.length;
+      const prevOption = props.options[currentId - 1];
+
+      hover.value = prevOption ? prevOption[props.idKey] : null;
+    }
+
     const keyPressHandler = (event: KeyboardEvent) => {
-      if (event.key === 'Tab') {
+      if (event.key === 'Tab' || event.key === 'Escape') {
         blurFocus();
+      } else if (event.key === 'ArrowDown') {
+        hoverNext();
+      } else if (event.key === 'ArrowUp') {
+        hoverPrev();
       } else if (event.key === 'Enter' && event.ctrlKey) {
         addElement();
+      } else if (event.key === 'Enter' && hover.value) {
+        select(hover.value);
       }
     }
 
@@ -99,10 +125,12 @@ export default defineComponent({
       id,
       focus,
       selected,
+      hover,
       select,
-      setFocusToInput,
+      setFocus,
       keyPressHandler,
       addElement,
+      hoverOn,
     }
   }
 });
