60c3bc0f5f31bb4a09804140dc9b3192707b0032
mansur <yaponomat@gmail.com>
2021-02-16 09:41:53 +0300

    Remove options from course

--- resources/js/backend/components/SelectOptions.vue
+++ resources/js/backend/components/SelectOptions.vue
@@ -16,11 +16,13 @@
       </div>
 
       <div class="bg-gray-700 p-3 flex">
-        <input type="text" class="bg-white rounded-tl rounded-bl border border-light focus:ring w-full" v-model="option.price">
+        <input type="text" class="bg-white rounded-tl rounded-bl border border-light focus:ring min-w-0" v-model="option.price">
         <div class="bg-gray-100 px-2 border-l border-gray-300 rounded-tr rounded-br">руб</div>
       </div>
+
+      <button class="bg-primary-light text-light py-2" @click="deleteOption(option.option)">Удалить</button>
     </div>
-    <create-option @create="optionCreate" />
+    <create-option @create="createOption" />
   </div>
 </template>
 
@@ -42,15 +44,21 @@ export default defineComponent({
   },
   emits: [ 'update:modelValue' ],
   setup(props, { emit }) {
-    const optionCreate = (option: Option) => {
+    const createOption = (option: Option) => {
       const newOptions = [...props.modelValue];
       newOptions.push({ option: option, price: 0.0 });
 
       emit('update:modelValue', newOptions);
     }
 
+    const deleteOption = (option: Option) => {
+      const newOptions = props.modelValue.filter(o => o.option.id !== option.id)
+      emit('update:modelValue', newOptions);
+    }
+
     return {
-      optionCreate
+      createOption,
+      deleteOption
     }
   }
 });
