7221c40c44263102d59b18da3d607cd48d58ba78
mansur <yaponomat@gmail.com>
2021-02-25 16:27:06 +0300

    Modify course options styling

--- resources/views/components/courses/options.blade.php
+++ resources/views/components/courses/options.blade.php
@@ -1,10 +1,10 @@
 <?php /** @var App\Models\Option[] $options */ ?>
 <div class="text-center">
-  <div class="inline-flex flex-col lg:flex-row mx-auto lg:shadow-lg lg:border border-gray-600 overflow-hidden lg:rounded-lg text-left">
+  <div class="inline-flex flex-col lg:flex-row mx-auto lg:shadow-lg lg:border border-gray-400 lg:overflow-hidden lg:rounded-lg text-left">
     @foreach($options as $option)
       <div class="w-72 flex flex-col mb-6 lg:mb-0 rounded-xl lg:rounded-none shadow-lg lg:shadow-none
                   overflow-hidden border border-gray-600 lg:border-t-0 lg:border-l-0 lg:border-b-0 lg:border-r
-                  lg:last:border-0 lg:border-gray-300">
+                  lg:last:border-0 lg:border-gray-400">
         <div class="bg-gray-700 p-4 text-light">{{ $option->name }}</div>
 
         <div class="p-4 flex-grow">
@@ -19,6 +19,12 @@
           @endforeach
         </div>
 
+        @if($option->caption)
+        <div class="p-4">
+          <p>* <em>{{ $option->caption }}</em></p>
+        </div>
+        @endif
+
         <div class="p-4 text-center">
           @if($option->pivot->price > 0)
             <span class="text-4xl font-head font-semibold text-primary">{{ $option->pivot->price }}</span> руб.
@@ -27,7 +33,7 @@
           @endif
         </div>
 
-        <a href="" class="block bg-red-500 text-light text-center py-4 uppercase hover:bg-red-600">Записаться на курс</a>
+        <a href="" class="block bg-red-400 text-light text-center py-4 uppercase hover:bg-red-500">Записаться на курс</a>
       </div>
     @endforeach
   </div>
