bb6d6f279e09d7a2a18be795eb3a32db8eabb199
mansur <yaponomat@gmail.com>
2021-02-26 09:31:30 +0300

    Course options styling

--- resources/views/components/courses/options.blade.php
+++ resources/views/components/courses/options.blade.php
@@ -1,11 +1,9 @@
 <?php /** @var App\Models\Option[] $options */ ?>
-<div class="text-center">
-  <div class="inline-flex flex-col lg:flex-row mx-auto lg:shadow-lg lg:border border-gray-400 lg:overflow-hidden lg:rounded-lg text-left">
+<div>
+  <div class="inline-flex flex-col lg:flex-row">
     @foreach($options as $option)
-      <div class="w-72 flex flex-col mb-6 lg:mb-0 rounded-xl lg:rounded-none shadow-lg lg:shadow-none
-                  overflow-hidden border border-gray-600 lg:border-t-0 lg:border-l-0 lg:border-b-0 lg:border-r
-                  lg:last:border-0 lg:border-gray-400">
-        <div class="bg-gray-700 p-4 text-light">{{ $option->name }}</div>
+      <div class="flex flex-1 flex-col mb-6 mx-3 lg:mb-0 p-4 border border-gray-400">
+        <div class="bg-gray-200 p-4 text-dark text-center">{{ $option->name }}</div>
 
         <div class="p-4 flex-grow">
           @foreach($option->capacities as $capacity)
@@ -27,13 +25,13 @@
 
         <div class="p-4 text-center">
           @if($option->pivot->price > 0)
-            <span class="text-4xl font-head font-semibold text-primary">{{ $option->pivot->price }}</span> руб.
+            <span class="text-4xl font-semibold">{{ number_format($option->pivot->price, 0, ',', ' ') }} ₽</span>
           @else
-            <span class="text-4xl font-head font-semibold text-primary">Бесплатно</span>
+            <span class="text-4xl font-semibold">Бесплатно</span>
           @endif
         </div>
 
-        <a href="" class="block bg-red-400 text-light text-center py-4 uppercase hover:bg-red-500">Записаться на курс</a>
+        <a href="" class="block bg-red-500 hover:bg-red-600 w-2/3 mx-auto p-3 text-white text-center">Записаться на курс</a>
       </div>
     @endforeach
   </div>
