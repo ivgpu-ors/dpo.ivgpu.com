fadab23b30d0cbca738943f1bf341d904a705eee
mansur <yaponomat@gmail.com>
2021-03-02 12:46:33 +0300

    Change courses pack to use flexbox instead grid

--- resources/views/components/courses/pack.blade.php
+++ resources/views/components/courses/pack.blade.php
@@ -1,5 +1,5 @@
 <?php /** @var App\Models\Course[]|Illuminate\Support\Enumerable $courses */ ?>
-<section class="grid lg:grid-cols-3 xl:grid-cols-4 gap-8 mb-16">
+<section class="flex flex-wrap mb-16 courses-grid">
   @foreach($courses as $course)
     <x-courses.card :course="$course" />
   @endforeach
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AccountController;
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\OrderController;
@@ -19,15 +20,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', HomeController::class);
 Route::get('/courses/{course_id}', [CourseController::class, 'show'])->name('courses.show');
 
-Route::middleware(['auth', 'member', 'can:study'])->group(function() {
+Route::middleware(['auth', 'member'])->group(function() {
     Route::get('/courses/{course_id}/signup/{option_id}', [CourseController::class, 'signup'])->name('courses.signup');
     Route::post('/courses/{course_id}/signup/{option_id}', [CourseController::class, 'order'])->name('courses.order');
+});
 
+Route::middleware(['auth', 'can:study'])->group(function() {
     Route::get('/order/success', [OrderController::class, 'success']);
     Route::get('/order/fail', [OrderController::class, 'fail']);
-    Route::get('/order/{order}', [OrderController::class, 'register'])
-        ->middleware('can:view-order,order')
-        ->name('order.register');
+    Route::get('/order/{order}', [OrderController::class, 'register'])->middleware('can:view-order,order')->name('order.register');
+
+    Route::get('/account', [AccountController::class, 'orders'])->name('account.orders');
 });
 
 Route::view('admin{any}', 'backend.app')->where('any', '.*')->middleware('auth');
